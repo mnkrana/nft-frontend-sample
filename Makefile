024233092880262f073b3241bcd60a059523f4b8
Mayank <mnk.rana@gmail.com>
2025-03-19 09:47:14 +0530

    chore(linter): remove elint

---
+++ Makefile
@@ -0,0 +1,2 @@
+format:
+	yarn prettier --write .
--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true, // Disable ESLint completely
+  },
   webpack: (config) => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
 };
